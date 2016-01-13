9d26ce91b18f38f80fb960d6e16b003a4aff4df8
jianing520 <mr.jianing@hotmail.com>
2016-01-13 17:18:41 +0100

    Added uploadsupport, automatically update the data from the form into mysql database

---
+++ Toestellenbeheer/Toestellenbeheer/Manage/add-hardware.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="Add a hardware" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="add-hardware.aspx.cs" Inherits="Toestellenbeheer.Manage.add_hardware" %>
+<asp:Content ID="addHardware" ContentPlaceHolderID="MainContent" runat="server">
+
+            <div id="add-hardware">
+        <fieldset>
+            <legend>Voer de nodige gegevens in.</legend>
+           <label for="type">type</label>&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server">
+            </asp:DropDownList>
+            <br />
+            <label for="Manufacturer">Manufacturer<asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            </label>
+                <br />
+            <label for="purchasedate">Purchase date</label>
+                <asp:Calendar ID="purchasedateCalendar" runat="server"></asp:Calendar>
+            <br />
+                
+            <label for="Serialnr">Serial Number</label>
+           <asp:TextBox id="Serialnr" runat="server"  placeholder="serial number can be found on the box of the product" /> <br />
+
+            <label for="internalNr">Internal Number</label>
+            <asp:TextBox runat="server" id="internalNr" placeholder="internal number can be found under the hardware" /> <br />
+
+            <label for="Picture">Picture -&nbsp; </label> add a picture - select one from the computer.
+            <asp:FileUpload ID="PictureUpload" runat="server" />
+            <asp:Button ID="Upload" runat="server" Text="Upload" OnClick="Upload_Click" />
+            <asp:Label ID="ResultUploadAtt" runat="server" Text=""></asp:Label>
+            <asp:Label ID="Testlocation" runat="server" Text=""></asp:Label>
+            <br />
+            
+
+            <label for="warrantyInfo">Warranty information</label>
+            <asp:TextBox id="warrantyInfo" runat="server" placeholder="internal number can be found under the hardware" Width="128px" /> <br />
+
+            <label for="extraInfo">Extra information</label>
+            <asp:TextBox runat="server" id="extraInfo" placeholder="internal number can be found under the hardware" /> <br />
+
+
+            <label for="attachments">Add attachments</label>&nbsp;
+            <asp:FileUpload ID="AttachmentUpload" runat="server" />
+            <asp:Button ID="UploadAttachment" runat="server" Text="Upload" OnClick="UploadAttachment_Click" />
+            <asp:Label ID="ResultUploadAtta" runat="server" Text=""></asp:Label>
+            <asp:Label ID="TestlocationAtt" runat="server" Text=""></asp:Label>
+            <br />
+
+           <asp:Button ID="Submit" runat="server" Text="Add hardware" OnClick="Submit_Click" />
+            <asp:TextBox id="test" runat="server"></asp:TextBox>
+        </fieldset>
+    </div>
+</asp:Content>
---
+++ Toestellenbeheer/Toestellenbeheer/Manage/add-hardware.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Toestellenbeheer.Manage
+{
+    public partial class add_hardware : System.Web.UI.Page
+    {
+        #region MySqlConnection Connection and Page Lode
+        //MySqlConnection mysqlConnectie = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        MySqlConnection mysqlConnectie = new MySqlConnection("Server = localhost; Port=3306;Database=Toestellenbeheer;Uid=root;Pwd=JIANINg****520");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        #region Insert Data
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            String strSerialNr = Serialnr.Text;
+            String strWarrantyInfo = warrantyInfo.Text;
+            String strInternalNr = internalNr.Text;
+            String strExtrainfo = extraInfo.Text;
+            String mImagePath = Testlocation.Text;
+            String mAttPath = TestlocationAtt.Text;
+
+            int dtePurchaseYear = purchasedateCalendar.SelectedDate.Year;
+            int dtePurchaseMonth = purchasedateCalendar.SelectedDate.Month;
+            int dtePurchaseDay = purchasedateCalendar.SelectedDate.Day;
+            String dtePurchaseDate = dtePurchaseYear.ToString() + '-' + dtePurchaseMonth.ToString() + '-' + dtePurchaseDay.ToString();
+            String dteAddedDate = DateTime.Now.ToString("yyyy-MM-dd");
+            try
+            {
+                /*
+                    MySqlCommand addHIEP= new MySqlCommand("Insert into hardware (purchaseDate, serialNr, internalNr, pictureLocation, warranty, extraInfo, attachmentLocation) values (@purchaseDate, @serialNr, @internalNr, @picLocation, @warranty, @extraInfo, @attLocation)", mysqlConnectie);
+                    addHIEP.Parameters.AddWithValue("@purchaseDate", dtePurchaseDate);
+                    addHIEP.Parameters.AddWithValue("@serialNr", strSerialNr);
+                    addHIEP.Parameters.AddWithValue("@internalNr", strExtraInfo);
+                    addHIEP.Parameters.AddWithValue("@picLocation", strExtraInfo);
+                    addHIEP.Parameters.AddWithValue("@warranty", strWarrantyInfo);
+                    addHIEP.Parameters.AddWithValue("@extraInfo", strExtraInfo);
+                    addHIEP.Parameters.AddWithValue("@attLocation", strExtraInfo);
+                    */
+                mysqlConnectie.Open();
+                
+                MySqlCommand addHIEP = new MySqlCommand("Insert into hardware (purchaseDate, serialNr, internalNr, pictureLocation, warranty, extraInfo, attachmentLocation, addedDate) values (@purchaseDate, @serialNr, @internalNr, @picLocation, @warranty, @extraInfo, @attLocation, @addedDate)", mysqlConnectie);
+                addHIEP.Parameters.AddWithValue("@purchaseDate", dtePurchaseDate);
+                addHIEP.Parameters.AddWithValue("@serialNr", strSerialNr);
+                addHIEP.Parameters.AddWithValue("@internalNr", internalNr);
+                addHIEP.Parameters.AddWithValue("@picLocation", mImagePath);
+                addHIEP.Parameters.AddWithValue("@warranty", strWarrantyInfo);
+                addHIEP.Parameters.AddWithValue("@extraInfo", strExtrainfo);
+                addHIEP.Parameters.AddWithValue("@attLocation", mAttPath);
+                addHIEP.Parameters.AddWithValue("@addedDate", dteAddedDate);
+
+                addHIEP.ExecuteNonQuery();
+                addHIEP.Dispose();
+            }
+            catch (MySqlException ex)
+            {
+                ShowMessage(ex.Message);
+            }
+            test.Text = dtePurchaseDate.ToString();              
+           }
+
+        void ShowMessage(string msg)
+        {
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<scriptlanguage = 'javascript'><script> alert('" + msg + "');</ script > ");
+        }
+
+        #endregion
+
+        public void Upload_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                Boolean fileOK = false;
+                String path = Server.MapPath("~/UserUploads/Images/");
+                if (PictureUpload.HasFile)
+                {
+                    String fileExtension =
+                        System.IO.Path.GetExtension(PictureUpload.FileName).ToLower();
+                    String[] allowedExtensions =
+                        {".gif", ".png", ".jpeg", ".jpg",".bmp",".tif"};
+                    for (int i = 0; i < allowedExtensions.Length; i++)
+                    {
+                        if (fileExtension == allowedExtensions[i])
+                        {
+                            fileOK = true;
+                        }
+                    }
+                }
+
+                if (fileOK)
+                {
+                    try
+                    {
+                        PictureUpload.PostedFile.SaveAs(path
+                            + AttachmentUpload.FileName);
+                       String mImagePath = path.ToString() + PictureUpload.FileName.ToString();
+                        Testlocation.Text = mImagePath;
+                        ResultUploadAtt.Text = "File uploaded!";
+                    }
+                    catch (Exception ex)
+                    {
+                        ResultUploadAtt.Text = "File could not be uploaded.";
+                    }
+                }
+                else
+                {
+                    ResultUploadAtt.Text = "Not a extension of image";
+                }
+            }
+        }
+
+        protected void UploadAttachment_Click(object sender, EventArgs e)
+        
+        {
+            if (IsPostBack)
+            {
+                String path = Server.MapPath("~/UserUploads/Attachment/");
+                if (AttachmentUpload.HasFile)
+                {
+                    try
+                    {
+                        AttachmentUpload.PostedFile.SaveAs(path
+                            + AttachmentUpload.FileName);
+                        String mAttachPath = path.ToString() + PictureUpload.FileName.ToString();
+                        TestlocationAtt.Text = mAttachPath;
+                        ResultUploadAtta.Text = "File uploaded!";
+                    }
+                    catch (Exception ex)
+                    {
+                        ResultUploadAtta.Text = "File could not be uploaded.";
+                    }
+                }
+            }
+            else
+            {
+                ResultUploadAtta.Text = "Do you not want to add a attachment?";
+            }
+            }
+        }
+    }
+    
+    

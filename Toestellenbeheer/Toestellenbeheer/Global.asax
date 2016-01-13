0ba0141f554235ba3ac335c800d6cffa5d63f7f2
jianing520 <mr.jianing@hotmail.com>
2016-01-13 11:21:19 +0100

    CHANGED FROM SQL TO MYSQL AS DEFAULT DB

---
+++ Toestellenbeheer/Toestellenbeheer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Toestellenbeheer.Global" Language="C#" %>
---
+++ Toestellenbeheer/Toestellenbeheer/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Toestellenbeheer.Models;
+
+[assembly: OwinStartupAttribute(typeof(Toestellenbeheer.Startup))]
+namespace Toestellenbeheer
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
